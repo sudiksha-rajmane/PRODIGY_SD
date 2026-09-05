1355b1d381b2ddbbffc1934ccdf6117268cd6f6b
Sudiksha Rajmane <sudiksharajmane06@gmail.com>
2026-09-05 11:27:46 +0530

    Add Contact class with basic attributes and methods

--- PRODIGY_SD_TASK03/Contact.java
+++ PRODIGY_SD_TASK03/Contact.java
@@ -1,1 +1,44 @@
+package PRODIGY_SD_TASKS;
 
+public class Contact {
+
+    private String name;
+    private String phone;
+    private String email;
+
+    public Contact(String name, String phone, String email) {
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void displayContact() {
+        System.out.println("Name  : " + name);
+        System.out.println("Phone : " + phone);
+        System.out.println("Email : " + email);
+    }
+}

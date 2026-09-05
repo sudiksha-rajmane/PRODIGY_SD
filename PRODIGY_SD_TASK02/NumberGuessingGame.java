1b7871171a66f49b74762e57dd76a84c9dcf7f34
Sudiksha Rajmane <sudiksharajmane06@gmail.com>
2026-09-05 11:20:35 +0530

    Implement number guessing game logic

--- PRODIGY_SD_TASK02/NumberGuessingGame.java
+++ PRODIGY_SD_TASK02/NumberGuessingGame.java
@@ -1,1 +1,45 @@
+package PRODIGY_SD_TASKS;
 
+import java.util.Random;
+import java.util.Scanner;
+
+public class NumberGuessingGame {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        Random random = new Random();
+
+        int number = random.nextInt(100) + 1;
+        int guess;
+        int attempts = 0;
+
+        System.out.println("=================================");
+        System.out.println("       NUMBER GUESSING GAME");
+        System.out.println("=================================");
+        System.out.println("I have selected a number between 1 and 100.");
+        System.out.println("Try to guess it!");
+
+        do {
+            System.out.print("\nEnter your guess: ");
+            guess = sc.nextInt();
+            attempts++;
+
+            if (guess > number) {
+                System.out.println("Too High! Try again.");
+            } 
+            else if (guess < number) {
+                System.out.println("Too Low! Try again.");
+            } 
+            else {
+                System.out.println("\nCongratulations! 🎉");
+                System.out.println("You guessed the number in " + attempts + " attempts.");
+            }
+
+        } while (guess != number);
+
+        System.out.println("=================================");
+
+        sc.close();
+    }
+}
